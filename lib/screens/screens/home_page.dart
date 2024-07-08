834283f9164442cefb6e1f89b1a81adcbddd5b45
Abd Elhalim Wagih <halemwagih@gmail.com>
2024-07-08 16:44:52 +0300

    add Click for Drawer

--- lib/screens/screens/home_page.dart
+++ lib/screens/screens/home_page.dart
@@ -2,19 +2,37 @@
 import 'package:flutter/material.dart';
 import 'package:respoonsize_1/screens/widget/custom_drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  GlobalKey<ScaffoldState> key = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const CustomDrawer(),
+      key: key,
+        backgroundColor: const Color(0xffDBDBDB),
+        drawer: const CustomDrawer(),
         appBar: AppBar(
-          title: const Text("Responive"),
+          leading: GestureDetector(
+            onTap: () {
+              key.currentState?.openDrawer();
+            },
+              child: const Icon(Icons.menu,color: Colors.white,)
+          ),
+          backgroundColor: Colors.black,
+          title: const Text("Responsive",style: TextStyle(color: Colors.white),),
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text("Welcome to My App"),
+        body: CustomScrollView(
+          slivers: [
+
+          ],
         )
     );
   }
